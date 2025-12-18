f59ed52d430917173321cba03c1c2294287b6d2e
Riya Rani <riyarani.core33@gmail.com>
2025-12-18 13:18:42 +0530

    add control panel with array size and regenerate

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,37 @@ import Navbar from "./components/Navbar";
 import ControlPanel from "./components/ControlPanel";
 import VisualizerCanvas from "./components/VisualizerCanvas";
 import InfoPanel from "./components/InfoPanel";
+import { useState, useEffect } from "react";
 
 export default function App() {
+  const createRandomArray = (size) =>
+    Array.from({ length: size }, () =>
+      Math.floor(Math.random() * 300) + 20
+    );
+
+  const [arraySize, setArraySize] = useState(50);
+  const [array, setArray] = useState(() => createRandomArray(50));
+
+  const generateArray = (size = arraySize) => {
+    setArray(createRandomArray(size));
+  };
+
+
   return (
     <div className="min-h-screen bg-slate-900 text-white">
       <Navbar />
 
-      <div className="p-4 grid grid-cols-1 lg:grid-cols-12 gap-4">
+      <div className="p-4 grid grid-cols-1 lg:grid-cols-12 gap-4 items-start">
         <div className="lg:col-span-3 h-full">
-          <ControlPanel />
+          <ControlPanel
+            arraySize={arraySize}
+            setArraySize={setArraySize}
+            generateArray={generateArray}
+          />
         </div>
 
         <div className="lg:col-span-6 h-full">
-          <VisualizerCanvas />
+          <VisualizerCanvas array={array} />
         </div>
 
         <div className="lg:col-span-3 h-full">
--- src/components/ControlPanel.jsx
+++ src/components/ControlPanel.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
 
-const ControlPanel = () => {
+export default function ControlPanel ({ arraySize, setArraySize, generateArray }) {
   return (
     <div className="h-full bg-slate-800 rounded-xl p-4 text-center">
-      ControlPanel
-    </div>
-  )
+      <h2 className="text-xl font-semibold"> Controls </h2>
+
+      <div>
+        <label className="block mb-2 text-sm">
+          Array Size: <span className="font-bold"> { arraySize } </span>
+        </label>
+
+        <input
+          type="range"
+          min="10"
+          max="100"
+          value={arraySize}
+          onChange={(e) => {
+            const size = Number(e.target.value);
+            setArraySize(size);
+            generateArray(size);
+          }}
+          className="w-full"
+        />
+      </div>
+
+      <button
+        onClick={() => generateArray()}
+        className="w-full bg-sky-500 hover:bg-sky-600 transition rounded-lg py-2 font-medium">
+        Generate New Array
+      </button>
+   </div>
+  );
 }
 
-export default ControlPanel
