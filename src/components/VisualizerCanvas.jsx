8082a5114ecf997a49f6a997c6f22230ec155a18
Riya Rani <riyarani.core33@gmail.com>
2025-12-18 13:16:18 +0530

    render array bars in visualizer

--- src/components/VisualizerCanvas.jsx
+++ src/components/VisualizerCanvas.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import { useState, useEffect } from "react";
 
-const VisualizerCanvas = () => {
+export default function VisualizerCanvas ({ array }) {
   return (
-    <div className="h-full bg-slate-800 rounded-xl p-4 flex items-end justify-center">
-      VisualizerCanvas
+    <div className="h-[400px] bg-slate-800 rounded-xl p-4 flex flex-col gap-4 overflow-hidden">
+      <h2 className="text-xl font-semibold text-center">
+        Visualizer
+      </h2>
+
+      <div className="flex-1 flex items-end justify-center gap-1">
+        {array.map((value, idx) => (
+          <div
+            key={idx}
+            className="bg-sky-400 rounded-sm transition-all"
+            style={{
+              height: `${Math.min(value, 360)}px`,
+              width: "10px",
+            }}
+          />
+        ))}
+      </div>
     </div>
   )
 }
 
-export default VisualizerCanvas
